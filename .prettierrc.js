18655ea36eb6ce2d10389a0f2641878fc23e668f
strong-minsu <dlalstn5789@gmail.com>
2024-03-29 19:11:27 +0900

    Initial commit

---
+++ .prettierrc.js
@@ -0,0 +1,11 @@
+//.prettierrc.js
+module.exports = {
+  singleQuote: true,
+  semi: true,
+  bracketSpacing: true,
+  bracketSameLine: true,
+  useTabs: false,
+  tabWidth: 2,
+  printWidth: 80,
+  arrowParens: "avoid",
+};
