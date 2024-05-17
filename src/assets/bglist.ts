4aba2c04bcfe3e41507482015e93d567a1269371
strong-minsu <dlalstn5789@gmail.com>
2024-05-17 23:53:54 +0900

    chore: bgList 분리

---
+++ src/assets/bglist.ts
@@ -0,0 +1,12 @@
+export const bglist = [
+  '#B9B9B9',
+  'linear-gradient(90deg, #91FCF6 0%, #E4D7 100%)',
+  'linear-gradient(90deg, #F9A364 0%, #559BB1 100%)',
+  'linear-gradient(90deg, #83D6CC 0%, #EFA08B 100%)',
+  'linear-gradient(90deg, #A2CEA4 0%, #7355B1 100%)',
+  'linear-gradient(90deg, #243860 0%, #C5B5E7 100%)',
+  'linear-gradient(90deg, #F9647F 0%, #73B155 100%)',
+  'linear-gradient(90deg, #DC5757 0%, #B19D55 100%)',
+  'linear-gradient(90deg, #66749e 0%, #e6fc8f 100%)',
+  'linear-gradient(90deg, #7fb681 0%, #ca8acf 100%)',
+];
